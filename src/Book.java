c1fa0031fbc57acc7d1c8c0a9ffc0ae632cd75b7
Nahid Fahim <fahimbinimam@gmail.com>
2025-07-18 12:28:08 +0600

    updated book.java

--- src/Book.java
+++ src/Book.java
@@ -1,115 +1,38 @@
 import java.io.Serializable;
-import java.time.LocalDate;
 
-public class Book  implements Serializable, Comparable<Book> {
-    private static final long serialVersionUID = 1L;
-    private static int nextId = 1;
-
-    private final int bookId;
+public class Book implements Serializable {
+    private String id;
     private String title;
     private String author;
-    private String isbn;
     private boolean isAvailable;
-    private LocalDate borrowDate;
-    private int memberId;
 
-    //constructor
-    public Book(String title, String author, String isbn){
-        if(title == null || title.trim().isEmpty()){
-            throw new IllegalArgumentException("Title cannot be null or empty");
-        }
-        this.bookId = nextId++;
+    public Book(String id, String title, String author) {
+        this.id = id;
         this.title = title;
         this.author = author;
-        this.isbn = isbn;
         this.isAvailable = true;
     }
 
-    //Getter & Setter (Encapsulation)
-    public int getBookId(){
-        return bookId;
+    // Getters and setters
+    public String getId() {
+        return id;
     }
-
-    public void setTitle(String title){
-        this.title = title;
-    }
-    public String getTitle(){
+    public String getTitle() {
         return title;
     }
-
-    public void setAuthor(String author){
-        this.author = author;
-    }
-    public String getAuthor(){
+    public String getAuthor() {
         return author;
     }
-
-    public void setIsbn(String isbn){
-        this.isbn = isbn;
-    }
-    public String getIsbn(){
-        return isbn;
-        
-    }
-
-    public boolean isAvailable(){
+    public boolean isAvailable() {
         return isAvailable;
     }
-
-    public void setBorrowDate(LocalDate borrowDate)
-            {
-        this.borrowDate = borrowDate;
-    }
-    public LocalDate getBorrowDate(){
-        return borrowDate;
-    }
-
-    public int getMemberId(){
-        return memberId;
-    }
-
-    //BL method -- business logic
-    //for borrowing book method
-    public void borrowBook(int memberId){
-        if(!isAvailable){
-            throw new IllegalStateException("Book is already borrowed");
-        }
-        this.isAvailable = false; // after borrowing book no book is available
-        this.borrowDate = LocalDate.now();
-        this.memberId = memberId;
-    }
-    //return book method
-    public void returnBook(){
-        this.isAvailable = true;
-        this.borrowDate = null;
-        this.memberId = 0;
-    }
-
-    //comparable implementation for sorting
-    @Override
-    public int compareTo(Book other){
-        return this.title.compareToIgnoreCase(other.title);
+    public void setAvailable(boolean available) {
+        isAvailable = available;
     }
 
     @Override
     public String toString(){
-        return String.format("Book{id=%d, title='%s', author='%s', isbn='%s', available='%s'}",bookId, title, author, isbn, isAvailable);
+        return String.format("Book{ID:%s, Title:'%s', Author:'%s', Available:%s}", id, title, author, (isAvailable ? "Yes" : "No"));
     }
 
-    @Override
-    public boolean equals(Object obj){
-        if(this == obj){
-            return true;
-        }
-        if(obj == null || getClass() != obj.getClass(){
-            return false;
-        }
-        Book book = (Book) obj;
-        return bookId == book.bookId;
-    }
-
-    @Override
-    public int hashCode(){
-        return Integer.hashCode(bookId);
-    }
 }
--- src/Member.java
+++ src/Member.java
@@ -1,5 +1,55 @@
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 abstract class Member implements Serializable {
     public static final long serialVersisonUID =1L;
+    public static int nextId = 1;
+
+    protected final int memberId;
+    protected String name;
+    protected String email;
+    protected List<Integer> borrowedBooks;
+
+    //constructor
+    public Member(String name, String email){
+        this.memberId = nextId++;
+        this.name = name;
+        this.email = email;
+        this.borrowedBooks = new ArrayList<>();
+    }
+
+    //Getters
+    public int getMemberId(){
+        return memberId;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public List<Integer> getBorrowedBooks(){
+        return new ArrayList<>(borrowedBooks);
+    }
+
+    //abstract method - must be implemented in subclasses
+    public abstract int getMaxBooksAllowed();
+
+    //common functionality
+    public boolean canBorrowMoreBooks(){
+        return borrowedBooks.size() < getMaxBooksAllowed();
+    }
+    public void borrowBook(int bookId){
+        if(!canBorrowMoreBooks()){
+            throw new IllegalStateException("Member has reached maximum book limit");
+        }
+        borrowedBooks.add(bookId);
+    }
+
+    public void returnBook(int bookId){
+        borrowedBooks.remove(Integer.valueOf(BookId))
+    }
+
+
 }
