4d0efd8c19afb7b25a3e9c5b6988b346747ac169
Nahid Fahim <fahimbinimam@gmail.com>
2025-07-18 12:59:48 +0600

    added transaction

---
+++ src/Transaction.java
@@ -0,0 +1,45 @@
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class Transaction implements Serializable {
+    private String transactionId;
+    private Book book;
+    private Member member;
+    private LocalDate checkoutDate;
+    private LocalDate returnDate;
+
+    //constructor
+    public Transaction(String transactionId, Book book, Member member){
+        this.transactionId = transactionId;
+        this.book = book;
+        this.member = member;
+        this.checkoutDate = LocalDate.now();
+        this.returnDate = null;
+    }
+
+    //setter & getter
+    public String getTransactionId(){
+        return transactionId;
+    }
+    public Book getBook(){
+        return book;
+    }
+    public Member getMember(){
+        return member;
+    }
+    public LocalDate getCheckoutDate(){
+        return checkoutDate;
+    }
+    public LocalDate getReturnDate(){
+        return returnDate;
+    }
+
+    @Override
+    public String toString(){
+        return "Transaction Id: " + transactionId+
+                "\nBook: " + book.getTitle() +
+                "\nMember: " + member.getName() +
+                "\nCheckout Date: " + checkoutDate +
+                "\nReturn Date: " + (returnDate != null ? returnDate : "Not Regurned Yet");
+    }
+}
