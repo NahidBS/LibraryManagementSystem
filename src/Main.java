c90135d69cce680ed5a3c651dac0cdc890a56f60
Nahid Fahim <fahimbinimam@gmail.com>
2025-07-21 23:43:29 +0600

    updated main.java

--- src/Main.java
+++ src/Main.java
@@ -24,7 +24,60 @@ public class Main {
                 case 1:
                     addBook(Library, scanner);
                     break;
+
+                case 2:
+                    addMember(Library, scanner);
+                    break;
+                case 3:
+                    checkoutBook(library, scanner);
+                    break;
+                case 4:
+                    returnBook(library, scanner);
+                    break;
+                case 5:
+                    viewAllBooks(library);
+                    break;
+                case 6:
+                    viewAllMembers(library);
+                    break;
+                case 7:
+                    viewAllTransactions(library);
+                    break;
+                case 8:
+                    System.out.println("Exiting...");
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice. Please try again.");
             }
         }
     }
+    
+    
+    
+    private static void addBook(Library library, Scanner scanner){
+        System.out.print("Enter Book ID: ");
+        String id = scanner.nextLine();
+        System.out.print("Enter Book Title: ");
+        String title = scanner.nextLine();
+        System.out.print("Enter Book Author: ");
+        String author = scanner.nextLine();
+
+        Book book = new Book(id, title, author);
+        library.addBook(book);
+        System.out.println("Book added successfully!");
+
+    }
+
+    private static void addMember(Library library, Scanner scanner) {
+        System.out.print("Enter Member ID: ");
+        String id = scanner.nextLine();
+        System.out.print("Enter Member Name: ");
+        String name = scanner.nextLine();
+
+        Member member = new Member(id, name);
+        library.addMember(member);
+        System.out.println("Member added successfully!");
+    }
+
 }
